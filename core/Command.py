9456f2c55f8f9f021aca0d72809221a8a5e0a794
JangSeHun <westreed@naver.com>
2022-07-28 11:10:40 +0900

    ADD : Update Minecraft Info

--- core/Command.py
+++ core/Command.py
@@ -105,6 +105,26 @@ class Command(commands.Cog):
                 file = discord.File("text.txt")
                 await ctx.channel.send(f'실행 결과가 너무 길어서 파일로 출력했어요.')
                 await ctx.channel.send(file=file)
+
+    @commands.command(name="마크", aliases=['마인크래프트', '마크서버'])
+    async def 마크(self, ctx, *input):
+        embed=discord.Embed(color=0xB22222, title="산업모드서버", description="`갈대`의 1.18.2 포지서버", timestamp=ctx.message.created_at)
+        embed.set_thumbnail(url=ctx.author.avatar_url)
+        embed.add_field(name = '서버주소', value = 'westreed.kro.kr')
+        embed.add_field(name = '서버문의', value = '@갈대#2519 에게 연락하세요!')
+        embed.add_field(name = '서버모드', value = 'https://drive.google.com/drive/folders/1oT2hivIlCF9Cm5TIsFr3pK-vkvnw1zaT?usp=sharing')
+        embed.add_field(name = '상세정보', value = '상세한 설명은 "!갈대서버"를 입력하세요.')
+        embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
+        await ctx.channel.send(embed=embed)
+    
+    @commands.command(name="갈대서버")
+    async def 갈대서버(self, ctx, *input):
+        embed=discord.Embed(color=0xB22222, title="갈대서버 상세설명", description="모드파일 : https://drive.google.com/drive/folders/1oT2hivIlCF9Cm5TIsFr3pK-vkvnw1zaT?usp=sharing\n넣는경로 : C:\\Users\\\계정명\\AppData\\Roaming\\\.minecraft\\mods", timestamp=ctx.message.created_at)
+        embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
+        embed.add_field(name = '설치순서1', value = '다운받은 파일 중, forge-1.18.2-40.1.0-installer.jar을 실행하여 1.18.2 포지를 설치한다.')
+        embed.add_field(name = '설치순서2', value = 'userMods.zip을 마인크래프트 모드폴더에 풀어놓는다.')
+        embed.add_field(name = '설치순서3', value = '마크런처에서 설치된 1.18.2 포지를 켜서, westreed.kro.kr 주소로 서버 접속하기.')
+        await ctx.channel.send(embed=embed)
         
 
     
