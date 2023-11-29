229ccab4fe4b2365dfd6333b486847df7befc1f6
JangSeHun <westreed@naver.com>
2023-11-29 22:42:34 +0900

    fix: 예외처리 및 2페이지로 나눠서 표시

--- core/Youtube.py
+++ core/Youtube.py
@@ -8,8 +8,9 @@ class Youtube(commands.Cog):
     def __init__(self, bot):
         print(f'{type(self).__name__}가 로드되었습니다.')
         self.bot = bot
+        self.title = "유튜브 검색"
         self.list_count = 10
-        self.sel_emoji = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+        self.sel_emoji = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟", "⬅️", "➡️", "❌"]
 
     def search_videos(self, youtube, keyword):
         request = youtube.search().list(
@@ -27,61 +28,103 @@ class Youtube(commands.Cog):
     
     @staticmethod
     async def select_video(ctx, keyword, video):
-        _, video_url, video_info = video
-        await ctx.channel.send(f"### [ 🔮유튜브 검색완료 ]\n\n**{keyword} 의 검색 결과입니다.**\n{video_info}\n{video_url}")
+        _, video_url, video_info, _ = video
+        await ctx.reply(f"### [ 🔮유튜브 검색완료 ]\n\n**{keyword} 의 검색 결과입니다.**\n{video_info}\n{video_url}", mention_author=False)
     
     @commands.command(name="유튜브", aliases=["유튜브검색"])
     async def 유튜브(self, ctx, *input):
+        keyword = " ".join(input)
+        if keyword == "":
+            embed = discord.Embed(color=0xB22222, title="[ 🚨유튜브 검색오류 ]", description=f"검색어를 입력하셔야 합니다!")
+            embed.set_footer(text = f"{ctx.author.display_name} | {self.title}", icon_url = ctx.author.display_avatar)
+            msg = await ctx.reply(embed=embed)
+            await msg.delete(delay=5)
+            await ctx.message.delete(delay=5)
+            return
+
         tree = elemTree.parse('./keys.xml')
         SECRETKEY = tree.find('string[@name="YoutubeKey"]').text
         youtube = build('youtube', 'v3', developerKey=SECRETKEY)
 
-        keyword = " ".join(input)
-        url = f"https://www.youtube.com/results?search_query={keyword}"
 
         embed = discord.Embed(color=0xB22222, title="[ 🪄유튜브 검색중 ]", description=f"잠시만 기다려주세요!\n정보를 수집 중이므로 다소 시간이 걸릴 수 있습니다.")
-        embed.set_footer(text = f"{ctx.author.display_name}", icon_url = ctx.author.display_avatar)
+        embed.set_footer(text = f"{ctx.author.display_name} | {self.title}", icon_url = ctx.author.display_avatar)
         msg = await ctx.channel.send(embed=embed)
 
         try:
             videos = self.search_videos(youtube, keyword)
         except Exception as e:
-            embed = discord.Embed(color=0xB22222, title="[ 🚨유튜브 검색 오류 ]", description=f"유튜브 API에 문제가 발생했어요.\n`{e}`")
-            embed.set_footer(text = f"{ctx.author.display_name}", icon_url = ctx.author.display_avatar)
+            embed = discord.Embed(color=0xB22222, title="[ 🚨유튜브 검색오류 ]", description=f"유튜브 API에 문제가 발생했어요.\n`{e}`")
+            embed.set_footer(text = f"{ctx.author.display_name} | {self.title}", icon_url = ctx.author.display_avatar)
             await msg.edit(embed=embed)
 
         result = []
-        result_text = ""
         for idx, video in enumerate(videos):
             video_id = video['id']['videoId']
             title = video['snippet']['title']
             channelTitle = video['snippet']['channelTitle']
             video_url = self.get_video_url(video_id)
+            thumbnail_url = video['snippet']['thumbnails']['high']['url']
 
             video_info = f"{title} | {channelTitle}"
-            result.append((video_id, video_url, video_info))
-            result_text += f"{idx+1}번 {video_info}\n"
+            result.append((video_id, video_url, video_info, thumbnail_url))
         
-        embed = discord.Embed(color=0xB22222, title="[ 🪄유튜브 검색중 ]", description=f"**{keyword} 의 검색 결과입니다.**\n\n{result_text}")
-        embed.set_footer(text = f"{ctx.author.display_name}", icon_url = ctx.author.display_avatar)
-        await msg.edit(embed=embed)
-        for i in range(self.list_count):
-            await msg.add_reaction(self.sel_emoji[i])
+        paging = 0
+        half_count = self.list_count//2
+        while True:
+            # Youtube Search Texts
+            result_text = ""
+            for i in range(half_count) if paging == 0 else range(half_count, self.list_count):
+                result_text += f"{i+1}번 {result[i][2]}\n"
 
-        try:
-            def check(reaction, user):
-                return str(reaction) in self.sel_emoji and \
-                user == ctx.author and reaction.message.id == msg.id
-            reaction, user = await self.bot.wait_for('reaction_add', timeout=20.0, check=check)
+            embed = discord.Embed(color=0xB22222, title="[ 🪄유튜브 검색중 ]", description=f"**{keyword} 의 검색 결과입니다.**\n`아래의 이모지를 클릭해서 선택할 수 있습니다.`\n\n{result_text}")
+            embed.set_footer(text = f"{ctx.author.display_name} | {self.title}", icon_url = ctx.author.display_avatar)
+            await msg.edit(embed=embed)
 
-            for i in range(self.list_count):
-                if str(reaction) == self.sel_emoji[i]:
+            # 1 ~ 10
+            for i in range(half_count) if paging == 0 else range(half_count, self.list_count):
+                await msg.add_reaction(self.sel_emoji[i])
+            
+            # Next, Prev, Cancle
+            if paging == 0: await msg.add_reaction(self.sel_emoji[-2]) # Next
+            else:           await msg.add_reaction(self.sel_emoji[-3]) # Prev
+            await msg.add_reaction(self.sel_emoji[-1]) # Cancle
+
+            try:
+                def check(reaction, user):
+                    return str(reaction) in self.sel_emoji and \
+                    user == ctx.author and reaction.message.id == msg.id
+                reaction, user = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
+
+                for i in range(self.list_count):
+                    if str(reaction) == self.sel_emoji[i]:
+                        await msg.delete()
+                        await self.select_video(ctx, keyword, result[i])
+                        return
+                
+                if str(reaction) == self.sel_emoji[-3]: # prev
+                    paging = 0
+                    await msg.clear_reactions()
+                    continue
+                
+                elif str(reaction) == self.sel_emoji[-2]: # next
+                    paging = 1
+                    await msg.clear_reactions()
+                    continue
+                
+                elif str(reaction) == self.sel_emoji[-1]:
                     await msg.delete()
-                    await self.select_video(ctx, keyword, result[i])
-                    break
-        except asyncio.TimeoutError:
-            await msg.delete()
-            await self.select_video(ctx, keyword, result[0])
+                    embed = discord.Embed(color=0xB22222, title="[ 🪄유튜브 검색취소 ]", description=f"유튜브 검색을 취소했어요.")
+                    embed.set_footer(text = f"{ctx.author.display_name} | {self.title}", icon_url = ctx.author.display_avatar)
+                    msg2 = await ctx.reply(embed=embed)
+                    await msg2.delete(delay=5)
+                    await ctx.message.delete(delay=5)
+                    return
+                    
+            except asyncio.TimeoutError:
+                await msg.delete()
+                await self.select_video(ctx, keyword, result[0])
+                return
     
 
 async def setup(bot):
