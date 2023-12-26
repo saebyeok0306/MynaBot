30665664bdac5254b42b83d3bbbd308d8e1dd32e
JangSeHun <westreed@naver.com>
2023-12-26 23:22:33 +0900

    feat: 멀티서버 환경에서도 작동하도록 병렬처리

--- core/TTS.py
+++ core/TTS.py
@@ -1,4 +1,4 @@
-import discord, random
+import discord, random, asyncio
 import data.Functions as fun
 import data.Logs as logs
 from pathlib import Path
@@ -7,6 +7,12 @@ from discord.ext import commands, tasks
 from collections import deque, defaultdict
 from dotenv import dotenv_values
 
+class Chat:
+    def __init__(self):
+        self.timer = 0
+        self.message_queue = deque()
+        self.voice_client = None
+        self.voice_channel = None
 
 class TTS(commands.Cog):
 
@@ -14,57 +20,68 @@ class TTS(commands.Cog):
         print(f'{type(self).__name__}가 로드되었습니다.')
 
         self.bot = bot
-        self.voice_channel = {}
+        self.tts_channel = defaultdict(Chat)
         self.is_cat = defaultdict(bool)
-        self.message_queue = deque()
+        # self.voice_channel = {}
+        # self.message_queue = defaultdict(deque)
         self.file_path = "./data"
         self.message_queue_process.start()
     
     def cog_unload(self):
         self.message_queue_process.stop()
 
-    async def read_message(self):
+    async def read_message(self, guild_id):
         import os
         
         try:
-            if self.message_queue:
-                vc = self.message_queue[0].guild.voice_client
+            vc = self.tts_channel[guild_id].voice_client
 
-                if vc.is_playing():
-                    return
-
-                message = self.message_queue.popleft()
-
-                id = message.guild.id
-                file = f"{id}.mp3"
-                res = self.synthesize_text(file, message)
-                # res = self.openai_tts(file, message)
-                if res is True:
-                    vc.play(discord.FFmpegPCMAudio(source=f"{self.file_path}/{file}"), after= lambda x: os.remove(f"{self.file_path}/{file}"))
-                    self.voice_channel[id] = 0
+            if vc.is_playing():
+                return
             
-            else:
-                for id in self.voice_channel.keys():
-                    self.voice_channel[id] += 1
+            message = self.tts_channel[guild_id].message_queue.popleft()
 
-                    guild = self.bot.get_guild(id)
-                    channel = guild.voice_client.channel
-                    if guild.voice_client is not None and len(channel.members) == 1:
-                        await guild.voice_client.disconnect()
-                        del self.voice_channel[id]
-                        print(f"{guild.name} 서버의 음성채팅에서 봇이 자동으로 퇴장했습니다.")
-
-                    if self.voice_channel[id] > 600:
-                        if guild.voice_client is not None:
-                            await guild.voice_client.disconnect()
-                        del self.voice_channel[id]
-                        print(f"{guild.name} 서버의 음성채팅에서 봇이 자동으로 퇴장했습니다.")
+            id = message.guild.id
+            file = f"{id}.mp3"
+            res = self.synthesize_text(file, message)
+            # res = self.openai_tts(file, message)
+            if res is True:
+                vc.play(discord.FFmpegPCMAudio(source=f"{self.file_path}/{file}"), after= lambda x: os.remove(f"{self.file_path}/{file}"))
+                self.tts_channel[id].timer = 0
         except:
             pass
     
     @tasks.loop(seconds=1)
     async def message_queue_process(self):
-        await self.read_message()
+        message_tasks = []
+        delete_tts_channel = []
+        for guild_id in self.tts_channel.keys():
+            if self.tts_channel[guild_id].message_queue:
+                message_tasks.append(asyncio.create_task(self.read_message(guild_id)))
+
+            else:
+                self.tts_channel[guild_id].timer += 1
+                guild = self.bot.get_guild(guild_id)
+
+                # 변수에는 존재하는데 실제 봇은 입장을 안한 상태인 경우
+                if guild.voice_client is None:
+                    await self.tts_channel[guild_id].voice_channel.connect()
+
+                channel = guild.voice_client.channel
+                if len(channel.members) == 1:
+                    await guild.voice_client.disconnect()
+                    delete_tts_channel.append(guild_id)
+                    print(f"{guild.name} 서버의 음성채팅에서 봇이 자동으로 퇴장했습니다.")
+
+                elif self.tts_channel[guild_id].timer > 600:
+                    await guild.voice_client.disconnect()
+                    delete_tts_channel.append(guild_id)
+                    print(f"{guild.name} 서버의 음성채팅에서 봇이 자동으로 퇴장했습니다.")
+
+        await asyncio.gather(*message_tasks)
+
+        for guild_id in delete_tts_channel:
+            del self.tts_channel[guild_id]
 
     def is_allow_guild(self, ctx):
 
@@ -72,6 +89,7 @@ class TTS(commands.Cog):
             "유즈맵 제작공간" : 631471244088311840,
             "데이터베이스" : 966942556078354502,
             "강화대전쟁" : 1171793482441039963,
+            "마스에몽" : 948601885575741541,
         }
         if ctx.guild.id in allow_guilds.values():
             return True
@@ -89,11 +107,13 @@ class TTS(commands.Cog):
         if message.author.voice is None: return
         if message.author.voice.channel != vc.channel: return
         if message.content.startswith("!"): return
+        if message.content.startswith("http://"): return
+        if message.content.startswith("https://"): return
         if not message.channel.id in fun.getBotChannel(self.bot, message):
             if str(message.channel.type) != "voice": return
             if vc.channel != message.channel: return
 
-        self.message_queue.append(message)
+        self.tts_channel[message.guild.id].message_queue.append(message)
 
     @commands.command(name="TTS", aliases=["음성채팅입장", "음성입력", "TTS입장", "입장"])
     async def TTS(self, ctx, *input):
@@ -118,8 +138,9 @@ class TTS(commands.Cog):
         voice_channel = ctx.author.voice.channel
         await voice_channel.connect()
 
-        if ctx.guild.id not in self.voice_channel.keys():
-            self.voice_channel[ctx.guild.id] = 0
+        if ctx.guild.id not in self.tts_channel.keys():
+            self.tts_channel[ctx.guild.id].voice_client = ctx.guild.voice_client
+            self.tts_channel[ctx.guild.id].voice_channel = voice_channel
         
         await logs.SendLog(bot=self.bot, log_text=f"{ctx.guild.name}의 {ctx.author.display_name}님이 TTS 명령어를 실행했습니다.")
 
@@ -148,6 +169,9 @@ class TTS(commands.Cog):
         voice_channel = ctx.author.voice.channel
         await voice_channel.connect()
 
+        self.tts_channel[ctx.guild.id].voice_client = ctx.guild.voice_client
+        self.tts_channel[ctx.guild.id].voice_channel = voice_channel
+
     @commands.command(name="흑이체")
     async def 흑이체(self, ctx):
         if self.is_allow_guild(ctx) is False: return
@@ -163,6 +187,37 @@ class TTS(commands.Cog):
     # async def 퇴장(self, ctx):
     #     if ctx.voice_client is not None:
     #         await ctx.voice_client.disconnect()
+
+    # @staticmethod
+    # def co_seong_che(text):
+    #     text.replace("ㄹㅇㅋㅋ", "리얼키키").replace("ㅋㅋㄹㅃㅃ", "쿠쿠루삥뽕").replace("")
+
+    def preprocess_text(self, author, text):
+        import re
+        from emoji import core
+
+        print(f"synthesize_text : {text}")
+
+        # 1. 이모지를 먼저 제거합니다.
+        text = core.replace_emoji(text, replace="")
+
+        # 2. 남은 텍스트에서 디스코드 이모지 문자열 <:이모지:> 을 검사해서 제거합니다.
+        pattern = r'<(.*?)>'
+        matches = re.findall(pattern, text)
+        if matches:
+            for pat in matches:
+                text = text.replace(f"<{pat}>", "")
+        
+        text = text.strip()
+
+        # 모두 제거 후, 문자열이 공백이면 return 합니다.
+        if text == "": return False
+
+        # 흑이체 사용할 대상
+        if self.is_cat[author.id]:
+            text = self.cat_speech(text)
+
+        return text
     
     @staticmethod
     def cat_speech(text):
@@ -188,34 +243,16 @@ class TTS(commands.Cog):
         return " ".join(trans_text)
                 
     def openai_tts(self, file, message):
-        import re
-        from emoji import core
         try:
-
-            text = message.content
             author = message.author
-            print(f"synthesize_text : {text}")
-
-            # 1. 이모지를 먼저 제거합니다.
-            text = core.replace_emoji(text, replace="")
-
-            # 2. 남은 텍스트에서 디스코드 이모지 문자열 <:이모지:> 을 검사해서 제거합니다.
-            pattern = r'<(.*?)>'
-            matches = re.findall(pattern, text)
-            if matches:
-                for pat in matches:
-                    text = text.replace(f"<{pat}>", "")
-            
-            text = text.strip()
-            
-            # 모두 제거 후, 문자열이 공백이면 return 합니다.
-            if text == "": return False
-
-            # 흑이체 사용할 대상
-            if self.is_cat[author.id]:
-                text = self.cat_speech(text)
+            text = self.preprocess_text(author, message.content)
 
             text_length = len(text)
+            max_length = 200
+
+            # 문자열의 길이가 최대 길이보다 크면 return 합니다.
+            if text_length > max_length: return False
+
             speed = 2.0
             text_speed = [(10, 1.1), (20, 1.3), (30, 1.5), (40, 1.7)]
             for le, sp in text_speed:
@@ -238,44 +275,23 @@ class TTS(commands.Cog):
         except Exception as e:
             print(e)
             return False
-    
+
     def synthesize_text(self, file, message):
-        # "texttospeech import"
         from google.cloud import texttospeech
-        import re
-        from emoji import core
 
-        text = message.content
         author = message.author
-        print(f"synthesize_text : {text}")
+        text = self.preprocess_text(author, message.content)
 
-        # 1. 이모지를 먼저 제거합니다.
-        text = core.replace_emoji(text, replace="")
+        # text가 False이면 tts 취소하기
+        if text is False: return False
 
-        # 2. 남은 텍스트에서 디스코드 이모지 문자열 <:이모지:> 을 검사해서 제거합니다.
-        pattern = r'<(.*?)>'
-        matches = re.findall(pattern, text)
-        if matches:
-            for pat in matches:
-                text = text.replace(f"<{pat}>", "")
-        
-        text = text.strip()
-        
-        # 모두 제거 후, 문자열이 공백이면 return 합니다.
-        if text == "": return False
-        
         client = texttospeech.TextToSpeechClient()
         text_length = len(text)
         # 최대 길이를 200으로 지정 (지나치게 길어지면 에러 발생)
         max_length = 200
 
         # 문자열의 길이가 최대 길이보다 크면 return 합니다.
-        if  text_length > max_length: return False
-
-        # 흑이체 사용할 대상
-        if self.is_cat[author.id]:
-            text = self.cat_speech(text)
-
+        if text_length > max_length: return False
 
         # 텍스트 변환
         input_text = texttospeech.SynthesisInput(text=text)
@@ -297,7 +313,6 @@ class TTS(commands.Cog):
                 "pitch": 4.0
             }
         }
-
 
         # 오디오 설정 (예제에서는 한국어, 남성C)
         voice = texttospeech.VoiceSelectionParams(
