6f6ab0dcdf63f5e0c6b0e93d1cb174a8492a348b
JangSeHun <westreed@naver.com>
2024-02-05 17:40:52 +0900

    refactor: Discord VoiceClient와 관련된 모듈 리팩토링

--- core/VoiceClient.py
+++ core/VoiceClient.py
@@ -1,43 +1,112 @@
-import discord
-import utils.Utility as util
-import data.Logs as logs
+from collections import defaultdict
+
+import discord, asyncio
 from discord.ext import commands, tasks
 
+import utils.Utility as util
+import utils.Logs as logs
+from core.voice_client.TTS import TTS
+from core.voice_client.Music import Music
 
-class VoiceClient(commands.Cog):
+
+class State:
+    def __init__(self):
+        self.message_queue = []
+        self.is_music = False
+
+
+class VoiceClient(commands.Cog, TTS, Music):
 
     def __init__(self, bot):
         print(f'{type(self).__name__}가 로드되었습니다.')
+        for parent_class in self.__class__.__bases__:
+            if parent_class.__name__ == "Cog": continue
+            print(f'{parent_class.__name__}가 로드되었습니다.')
+
         self.bot = bot
+        self.voice_state = defaultdict(State)
+        self.delete_state_list = []
+        TTS.__init__(self, self.bot, self.voice_state)
+        Music.__init__(self, self.bot, self.voice_state)
         self.voice_client_processer.start()
 
     def cog_unload(self):
         self.voice_client_processer.stop()
 
-    async def voice_client_processer(self):
-        pass
-
     @commands.Cog.listener()
     async def on_message(self, message):
+        if not self.voice_client_processer.is_running():
+            # voice_client_processer Loop가 죽은 경우, 다시 실행하기
+            self.voice_client_processer.start()
         # if str(message.channel).startswith("Direct Message"): return
-        if message.author.bot: return None
+        if message.author.bot: return False
 
+        if self.voice_state[message.guild.id].is_music is True: return False
         vc = message.guild.voice_client
-        if vc is None: return
-        if message.author.voice is None: return
-        if message.author.voice.channel != vc.channel: return
-        if message.content.startswith("!"): return
-        if message.content.startswith("http://"): return
-        if message.content.startswith("https://"): return
+        if vc is None: return False
+        if message.author.voice is None: return False
+        if message.author.voice.channel != vc.channel: return False
+        if message.content.startswith("!"): return False
+        if message.content.startswith("http://"): return False
+        if message.content.startswith("https://"): return False
         if message.channel.id not in util.get_bot_channel(self.bot, message):
-            if str(message.channel.type) != "voice": return
-            if vc.channel != message.channel: return
-        # is_playing = db.GetMusicByGuild(message.guild)[1]
-        # if is_playing: return
+            if str(message.channel.type) != "voice": return False
+            if vc.channel != message.channel: return False
 
-        self.tts_channel[message.guild.id].message_queue.append(message)
-        if not self.message_queue_process.is_running():
-            self.message_queue_process.start()
+        self.voice_state[message.guild.id].message_queue.append(message)
+
+    @tasks.loop(seconds=1)
+    async def voice_client_processer(self):
+        """1초마다 루프하면서 voice_client와 관련된 모든 동작을 수행합니다."""
+        work_tasks = []
+        for voice_client in self.bot.voice_clients:
+
+            # bot.voice_clients에 등록된 길드가 voice_state에 없는 경우
+            if voice_client.guild.id not in self.voice_state.keys():
+                self.voice_state[voice_client.guild.id] = State()
+                continue
+
+            state = self.voice_state[voice_client.guild.id]
+
+            if state.is_music is True:
+                continue
+
+            if voice_client.is_playing():
+                continue
+            
+            # 플레이리스트에 음악이 존재하면
+            if self.exist_playlist(voice_client.guild):
+                work_tasks.append(
+                    asyncio.create_task(
+                        self.play_music(voice_client.guild)
+                    )
+                )
+                continue
+            
+            # Music의 플레이리스트가 없을 때 메시지 읽기
+            if state.message_queue:
+                work_tasks.append(
+                    asyncio.create_task(
+                        self.read_message_coroutine(voice_client.guild, voice_client)
+                    )
+                )
+                continue
+
+            if len(voice_client.channel.members) == 1:
+                self.delete_state_list.append(voice_client.guild.id)
+                print(f"{voice_client.guild.name} 서버의 음성채팅에서 봇이 자동으로 퇴장했습니다.")
+
+        asyncio.gather(*work_tasks)
+
+        for guild_id in self.delete_state_list:
+            try:
+                del self.voice_state[guild_id]
+                guild = self.bot.get_guild(guild_id)
+                if guild.voice_client:
+                    await guild.voice_client.disconnect()
+            except:
+                pass
+        self.delete_state_list = []
 
     @commands.command(name="입장", aliases=["음성채팅입장", "음성입력", "TTS입장"])
     async def 입장(self, ctx):
@@ -61,6 +130,10 @@ class VoiceClient(commands.Cog):
         if ctx.guild.voice_client:
             await ctx.guild.voice_client.disconnect()
         await ctx.author.voice.channel.connect()
+
+        # voice_state 상태 생성
+        if ctx.guild.id not in self.voice_state.keys():
+            self.voice_state[ctx.guild.id] = State()
 
         await logs.SendLog(bot=self.bot, log_text=f"{ctx.guild.name}의 {ctx.author.display_name}님이 입장 명령어를 실행했습니다.")
 
---
+++ core/voice_client/TTS.py
@@ -0,0 +1,175 @@
+from collections import defaultdict
+
+import discord, random
+from discord.ext import commands
+
+import utils.Logs as logs
+
+
+class TTS:
+
+    def __init__(self, bot, voice_state):
+        self.bot = bot
+        self.voice_state = voice_state
+        self.is_cat = defaultdict(bool)
+        self.file_path = "./data"
+
+    async def read_message_coroutine(self, guild, voice_client):
+        message = None
+        try:
+            message = self.voice_state[guild.id].message_queue.pop(0)
+
+            file = f"{guild.id}.mp3"
+            result = self.synthesize_text(file, message)
+
+            if type(result) is tuple and result[0] is False:
+                embed = discord.Embed(
+                    color=0xB22222, title="[ 🚨TTS 오류 ]",
+                    description=f"아래의 오류가 발생했습니다.\n{result[1]}"
+                )
+                embed.set_footer(text=f"{self.bot.user.name}", icon_url=self.bot.user.display_avatar)
+                await voice_client.channel.send(embed=embed)
+                # self.delete_tts_channel.append(guild_id)
+                return
+            if result is True:
+                voice_client.play(
+                    discord.FFmpegPCMAudio(source=f"{self.file_path}/{file}"),
+                    after=lambda x: __import__('os').remove(f"{self.file_path}/{file}")
+                )
+                # self.tts_channel[guild_id].timer = 0
+        except Exception as e:
+            await logs.SendLog(bot=self.bot, log_text=f"{guild.name}에서 {message.content}를 재생 하는데 실패했습니다.\nError: {e}")
+
+    @commands.command(name="흑이체")
+    async def 흑이체(self, ctx):
+        if ctx.author.voice is None:
+            return
+
+        author_id = ctx.author.id
+        self.is_cat[author_id] = not self.is_cat[author_id]
+
+        await ctx.reply(f"흑이체를 {'활성화' if self.is_cat[author_id] else '비활성화'}합니다.", mention_author=False)
+
+    @staticmethod
+    def cat_speech(text):
+        sentences = text.split(" ")
+
+        trans_text = []
+        for sentence in sentences:
+            l = len(sentence)
+            res = ""
+            if l == 1:
+                res = "냥"
+            else:
+                # 애옹, 야옹
+                l -= 1
+                res += random.choice(["애", "야", "먀"])
+
+                for _ in range(l - 1):
+                    l -= 1
+                    res += "오"
+
+                res += "옹"
+            trans_text.append(res)
+        return " ".join(trans_text)
+
+    def preprocess_text(self, author, text):
+        import re
+        from emoji import core
+
+        print(f"synthesize_text : {text}")
+
+        # 1. 이모지를 먼저 제거합니다.
+        text = core.replace_emoji(text, replace="")
+
+        # 2. 남은 텍스트에서 디스코드 이모지 문자열 <:이모지:> 을 검사해서 제거합니다.
+        pattern = r'<(.*?)>'
+        matches = re.findall(pattern, text)
+        if matches:
+            for pat in matches:
+                text = text.replace(f"<{pat}>", "")
+
+        text = text.strip()
+
+        # 모두 제거 후, 문자열이 공백이면 return 합니다.
+        if text == "":
+            return False
+
+        # 흑이체 사용할 대상
+        if self.is_cat[author.id]:
+            text = self.cat_speech(text)
+
+        return text
+
+    def synthesize_text(self, file, message):
+        from google.cloud import texttospeech
+
+        author = message.author
+        text = self.preprocess_text(author, message.content)
+
+        # text가 False이면 tts 취소하기
+        if text is False:
+            return False
+
+        client = texttospeech.TextToSpeechClient()
+        text_length = len(text)
+        # 최대 길이를 200으로 지정 (지나치게 길어지면 에러 발생)
+        max_length = 200
+
+        # 문자열의 길이가 최대 길이보다 크면 return 합니다.
+        if text_length > max_length: return False
+
+        # 텍스트 변환
+        input_text = texttospeech.SynthesisInput(text=text)
+
+        # 성별 선택
+        gender = "MALE"
+        if author.id in [298824090171736074, 369723279167979520, 413315617270136832, 389327234827288576,
+                         317960020912504832, 383483844218585108]:
+            gender = "FEMALE"
+
+        gender_info = {
+            "MALE": {
+                "name": "ko-KR-Neural2-C",
+                "ssml_gender": texttospeech.SsmlVoiceGender.MALE,
+                "pitch": 1.2
+            },
+            "FEMALE": {
+                "name": "ko-KR-Neural2-A",
+                "ssml_gender": texttospeech.SsmlVoiceGender.FEMALE,
+                "pitch": 4.0
+            }
+        }
+
+        # 오디오 설정 (예제에서는 한국어, 남성C)
+        voice = texttospeech.VoiceSelectionParams(
+            language_code="ko-KR",
+            name=gender_info[gender]["name"],
+            ssml_gender=gender_info[gender]["ssml_gender"],
+        )
+
+        speed = 2.0
+        text_speed = [(10, 1.1), (20, 1.3), (30, 1.5), (40, 1.7)]
+        for le, sp in text_speed:
+            if text_length <= le:
+                speed = sp
+                break
+
+        audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3,
+            speaking_rate=speed,
+            pitch=gender_info[gender]["pitch"]
+        )
+
+        try:
+            response = client.synthesize_speech(
+                request={"input": input_text, "voice": voice, "audio_config": audio_config}
+            )
+        except Exception as e:
+            return False, e
+
+        # audio 폴더 안에 output.mp3라는 이름으로 파일 생성
+        with open(f"{self.file_path}/{file}", "wb") as out:
+            out.write(response.audio_content)
+
+        return True
