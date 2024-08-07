74a22d456bf332719b55a6a920ff15125d3333d6
JangSeHun <westreed@naver.com>
2024-08-07 14:05:04 +0900

    refactor: Improve Windows test environment by disabling signal library when not on Linux OS

--- utils/Timeout.py
+++ utils/Timeout.py
@@ -1,6 +1,7 @@
 import errno
 import os
 import signal
+import platform
 from functools import wraps
 
 
@@ -11,16 +12,17 @@ class TimeoutError(Exception):
 def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
     def decorator(func):
         def _handle_timeout(signum, frame):
-            print('timeout')
             raise TimeoutError(error_message)
 
         def wrapper(*args, **kwargs):
-            signal.signal(signal.SIGALRM, _handle_timeout)
-            signal.setitimer(signal.ITIMER_REAL,seconds) #used timer instead of alarm
+            if platform.system() == 'Linux':
+                signal.signal(signal.SIGALRM, _handle_timeout)
+                signal.setitimer(signal.ITIMER_REAL,seconds) #used timer instead of alarm
             try:
                 result = func(*args, **kwargs)
             finally:
-                signal.alarm(0)
+                if platform.system() == 'Linux':
+                    signal.alarm(0)
             return result
         return wraps(func)(wrapper)
     return decorator
